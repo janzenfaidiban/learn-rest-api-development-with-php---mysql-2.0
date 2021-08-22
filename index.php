81dab3824d20298b2fda19c8ac0ea12ed6f96340
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-08-22 18:22:54 +0700

    18 Creating-Componen-1

--- index.php
+++ index.php
@@ -9,7 +9,40 @@
 </head>
 <body>
 
-    <h2>Hello World</h2>
+    <!-- Navigation Starts -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Article Hub</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">Registration</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Navigation Ends -->
+
+    <!-- Footer Starts -->
+    <footer class="footer">
+        <div class="container">
+            <span class="text-muted">Rest API Development Tutorial Udemy | Frank John.</span>
+        </div>
+    </footer>
+    <!-- Footer Ends -->
 
     <script src="assets/js/jquery.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
