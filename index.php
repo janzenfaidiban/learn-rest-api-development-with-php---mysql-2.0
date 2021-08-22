558ac0c9c8e7759c9f62d808da6347ca8d568a0f
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-08-22 18:10:23 +0700

    Installing Bootstrap

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Article Hub | Home</title>
+    <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+
+    <h2>Hello World</h2>
+
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/popper.js"></script>
+</body>
+</html>
