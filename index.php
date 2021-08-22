450b817b8e40531d6e93eec8966c98e859ba2c50
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-08-22 18:33:18 +0700

    19 Creating-Componen-2

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Article Hub | Home</title>
     <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
 
@@ -35,6 +36,17 @@
         </div>
     </nav>
     <!-- Navigation Ends -->
+
+    <!-- Jumbotron Starts -->
+    <div class="container m-top50">
+        <div class="h-100 p-5 bg-light border rounded-3">
+            <h2>Article Hub</h2>
+            <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+            <hr class="my-4">
+            <button class="btn btn-dark" type="button">Get Started</button>
+        </div>
+    </div>
+    <!-- Jumbotron Ends -->
 
     <!-- Footer Starts -->
     <footer class="footer">
