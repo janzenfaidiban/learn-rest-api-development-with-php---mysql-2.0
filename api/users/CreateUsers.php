0b5a140f6e57047fd24ae5fd6bff7e24a285786f
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-08-25 21:44:06 +0700

    API-End-Point-Create-User-1

---
+++ api/users/CreateUsers.php
@@ -0,0 +1,18 @@
+<?php 
+    // headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-type: ');
+    header('Assess-Control-Allow-Method: POST');
+    header('Access-Control-Allow-Headers: Origin, content-typ: Auth_key');
+
+    include_once '../../models/Apiusers.php';
+    include_once '../../models/Users.php';
+
+    // validate request 
+
+    // validate the content-type 
+
+    // validate auth_key 
+
+    // validate the param
+?>
